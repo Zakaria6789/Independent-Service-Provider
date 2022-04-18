20b5c53cf6da4f28d7bdc5acfd4ae8e3ccb91c02
Zakaria6789 <jakaria.hossain6363@gmail.com>
2022-04-18 06:51:53 +0600

    login form design ok

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import './Login.css';
+import facebook from '../../../images/social/facebook.png';
+import google from '../../../images/social/google.png';
+import github from '../../../images/social/github.png';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>This is the Login Page</h2>
+        <div className='container'>
+            <form className='login-container'>
+                <div className='login'>
+                    <div>
+                        <h2>Login Form</h2>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" placeholder='Email Address' />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder='Your Password' id="" />
+                        <a className='text-decoration-none' href="#reset">Forgot Password ?</a>
+                        <input type="submit" value="Login" />
+                        <p>Not a member ? <Link className='text-decoration-none' to='/resister'>Resister Now</Link></p>
+                    </div>
+                    <div className='or-container'>
+                        <div className='line'></div>
+                        <div className='or'>or</div>
+                        <div className='line'></div>
+                    </div>
+                    <div className='other-login'>
+                        <button>
+                            <img width='21' src={google} alt="" />
+                            Sign in With Google
+                        </button>
+                        <button>
+                            <img width='23' src={facebook} alt="" />
+                            Sign in With Facebook
+                        </button>
+                        <button>
+                            <img width='27' src={github} alt="" />
+                            Sign in With Github
+                        </button>
+
+                    </div>
+                </div>
+            </form>
+
         </div>
     );
 };
--- src/Pages/Login/Resister/Resister.js
+++ src/Pages/Login/Resister/Resister.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import google from '../../../images/social/google.png';
+import facebook from '../../../images/social/facebook.png';
+import github from '../../../images/social/github.png';
+import { Link } from 'react-router-dom';
 
 const Resister = () => {
     return (
-        <div>
-            <h2>This is the Resister page</h2>
+        <div className='container'>
+            <form className='login-container'>
+                <div className='login'>
+                    <div>
+                        <h2>Resister Form</h2>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="" placeholder='Your Name' />
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="" placeholder='Email Address' />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" placeholder='Your Password' id="" />
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <input type="password" name="confirmPassword" placeholder='Confirm Password' id="" />
+                        <input type="submit" value="Resister" />
+                        <p>Already member ? <Link className='text-decoration-none' to='/login'>Login Now</Link></p>
+                    </div>
+                    <div className='or-container'>
+                        <div className='line'></div>
+                        <div className='or'>or</div>
+                        <div className='line'></div>
+                    </div>
+                    <div className='other-login'>
+                        <button>
+                            <img width='21' src={google} alt="" />
+                            Sign in With Google
+                        </button>
+                        <button>
+                            <img width='23' src={facebook} alt="" />
+                            Sign in With Facebook
+                        </button>
+                        <button>
+                            <img width='27' src={github} alt="" />
+                            Sign in With Github
+                        </button>
+
+                    </div>
+                </div>
+            </form>
+
         </div>
     );
 };
